45b539427f025b24901a50cdd9ede54fa47d90eb
Lionel <lionel.repellin@gmail.com>
2017-08-23 17:06:20 +0200

    Add a test with static repository

---
+++ Business.Tests/UserServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using NFluent;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Tests
+{
+	[TestFixture]
+	public class UserServiceTests
+    {
+		private UserService _userService;
+
+		[SetUp]
+		public void Initialize()
+		{
+			_userService = new UserService();
+		}
+
+		[Test]
+		public void FindDatavivAccessAllowed()
+		{
+			var users = _userService.FindDatavivAccessAllowed();
+
+			Check.That(users)
+				.IsNotNull()
+				.And
+				.HasElementThatMatches(u => u.DatavivAccessAllowed);
+		}
+	}
+}
